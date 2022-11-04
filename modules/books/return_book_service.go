031eab64286f86b4081e0e064492d482897e9176
jirenmaa <ahmadalwiam@gmail.com>
2022-11-04 09:42:02 +0700

    fix return book

--- modules/books/return_book_service.go
+++ modules/books/return_book_service.go
@@ -1,12 +1,19 @@
 package books
 
+import "os"
+
 func ReturnBookService(isbn string) (string, bool) {
 	var isReturned bool = false
 	var messageErr string = "Something went terribly wrong. Are you certain that the ISBN is correct?"
 
+	hostname, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+
 	for index, book := range globalBook.Books {
 		// check if the borrower are actually the user and not the other
-		if book.Borrower == "YouAndMe" && book.ISBN == isbn {
+		if book.Borrower == hostname && book.ISBN == isbn {
 			isReturned = true
 			globalBook.Books[index].Borrower = "------"
 			globalBook.Books[index].Status = "Available"
